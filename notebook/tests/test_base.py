126ed77ea73c8b3f4e1277d6f76ff185d53ca785
Veronica Luke <veronica@intersect.org.au>
2015-01-08 12:45:54 +1100

    DIVERBEVAN-46 Added Mock http server in unit tests

--- notebook/tests/test_base.py
+++ notebook/tests/test_base.py
@@ -5,6 +5,7 @@ PROJECT_ROOT = os.path.dirname(__file__) + '/../'
 sys.path.insert(0, PROJECT_ROOT)
 
 import diver.config as config
+import diver.hiev as hiev
 
 # To run all tests from project directory:
 # python -m unittest discover -s tests/ -p 'test_*.py' -v
--- notebook/tests/test_search.py
+++ notebook/tests/test_search.py
@@ -1,4 +1,8 @@
 from test_base import *
+import requests
+# pip install sure, pip install httpretty
+from sure import expect
+import httpretty
 
 class SearchTestCase(unittest.TestCase):
     
@@ -7,6 +11,21 @@ class SearchTestCase(unittest.TestCase):
     
     def tearDown(self):
         return
+
+
     
-    def test_dummy(self):
-        return
+    @httpretty.activate
+    def test_search(self):       
+        
+        def request_callback(request, uri, headers):
+            return (200, headers, "The {} response from {}".format(request.method, uri))
+
+        
+        hiev.set_token('12345')
+        myhost = "http://somehost"
+        hiev.set_host(myhost)
+        myurl ='%s/%s?filename=myfile.egg&auth_token=12345&quiet=False' % (myhost, hiev.SEARCH_URL_FRAGMENT)
+        print myurl
+        httpretty.register_uri(httpretty.POST, myurl, body=request_callback)
+        hiev.search(filename="myfile.egg")
+        
